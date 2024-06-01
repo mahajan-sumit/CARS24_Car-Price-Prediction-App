895ea97d81ac915e0655f78708d6cc8c6ed9f875
mahajan-sumit <153998238+mahajan-sumit@users.noreply.github.com>
2024-06-01 17:31:13 +0530

    Update CARS24.py

--- CARS24.py
+++ CARS24.py
@@ -3,11 +3,11 @@ import streamlit as st
 import pickle
 
 # Load the dataset
-cars_df = pd.read_excel(r"cars24-car-price.xlsx")
+cars_df = pd.read_excel("cars24-car-price.xlsx")
 
 # Page configuration
 st.set_page_config(
-    page_title="CARS24: Car Price Prediction App",
+    page_title="Cars24: Car Price Prediction",
     page_icon="🚗",
     layout="wide",
     initial_sidebar_state="expanded",
@@ -43,7 +43,7 @@ encode_dict = {
 # Function to load the model and make predictions
 def model_pred(fuel_type, transmission_type, engine, seats, seller_type):
     # Load the model
-    with open(r"model.pkl", 'rb') as file:
+    with open("model.pkl", 'rb') as file:
         reg_model = pickle.load(file)
 
     # Encode categorical features
